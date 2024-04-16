fe061be483dce6937058d2fd1cd5a1a5fa75389a
mrborges-lncc <marcio.rentes.borges@gmail.com>
2024-04-16 12:40:38 -0300

    figures

---
+++ click_colab.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 16 11:57:53 2024
+
+@author: mrborges
+"""
+
+from pynput.mouse import Controller, Button
+import time
+
+mouse = Controller()
+
+while True:
+    mouse.click(Button.left,1)
+    print('clicked')
+    time.sleep(30)
